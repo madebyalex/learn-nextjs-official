9958dac4c6e4b27e884f0f632f3cb96fe3cb1a8e
Alexander Nuzhnyi <hello@alexander.works>
2024-05-05 19:07:40 +0200

    Init 🎉

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
+
+export default function Page() {
+    return (
+        <LatestInvoices />
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+// import { lusitana } from './ui/fonts';
+import Image from 'next/image'
+
+import styles from '@/app/ui/home.module.css'
+import variables from "@/app/ui/stylesheets/variables.module.scss"
+
 
 export default function Page() {
   return (
+    
     <main className="flex min-h-screen flex-col p-6">
+
+      {/* <div className={styles.shape} /> */}
+      {/* <h1 style={{color: variables.colorPrimary}}>Hello</h1> */}
+
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -17,6 +28,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          {/* <p className={`${lusitana.className} antialiased`}>Hello from Lusitana font! :) Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit repellat modi fugiat natus. Veniam temporibus similique quia pariatur illo architecto repudiandae, amet voluptatibus distinctio iusto voluptatum sapiente aliquid vero suscipit.</p> */}
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -26,6 +38,21 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className='hidden md:block'
+            alt="Screenshots of the dashboard project showing desktop version"
+          ></Image>
+
+          <Image
+            src='/hero-mobile.png'
+            width="560"
+            height='620'
+            className='block md:hidden'
+            alt="Screenshots of the dashboard project showing desktop version"
+          ></Image>
         </div>
       </div>
     </main>
